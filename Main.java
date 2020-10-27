760f3f870da21262b81273b62dd04746e8482f4c
mira-git <mirakorbel.20@gmail.com>
2020-10-28 00:51:09 +0100

    input protected by try-catch, added enum Operator

--- Main.java
+++ Main.java
@@ -4,32 +4,82 @@ import java.util.Locale;
 import java.util.Scanner;
 
 class SingleOperationCalculator {
+
+    public enum Operator {
+        ADD ('+'),
+        SUBTRACT ('-'),
+        MULTIPLY ('*'),
+        DIVIDE ('/');
+        private char symbol;
+
+        Operator(char symbol) {
+            this.symbol=symbol;
+        }
+
+        public static Operator findBySymbol(char symbol) {
+            Operator result = null;
+            for (Operator op : Operator.values()) {
+                if ( symbol == op.symbol ) {
+                    return op;
+                }
+            }
+            System.out.println("Operator "+symbol+" not recognized ...");
+            return null;
+        }
+
+        int calculate(int n1, int n2) {
+            int result = -1;
+            switch (this.ordinal()) {
+                case 0:
+                    result = n1 + n2; break;
+                case 1:
+                    result = n1 - n2; break;
+                case 2:
+                    result = n1 * n2; break;
+                case 3:
+                    result = n1 / n2; break;
+            }
+            return result;
+        }
+    }
+
     public static void main(String[] args) {
+
+        int first, second, result;
+        char operatorSymbol;
+        Operator o;
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
-        System.out.println("Enter two numbers: ");
-        System.out.print("first: ");
-        double first = scanner.nextDouble();
-        scanner.nextLine();
-        System.out.print("second: ");
-        double second = scanner.nextDouble(); // code duplicity
-        scanner.nextLine();
-        System.out.print("Enter an operator (+, -, *, /): ");
-        char operator = scanner.nextLine().charAt(0); // danger -> potential source of error
-        double result;
-        if (operator == '+') { // use switch
-            result = first + second;
-        } else if (operator == '-') {
-            result = first - second;
-        } else if (operator == '*') {
-            result = first * second;
-        } else if (operator == '/') {
-            result = first / second;
-        } else {
-            // operator doesn't match any case constant (+, -, *, /)
-            System.out.printf("Error! operator is not correct");
-            return;
+
+        while (true) {
+            System.out.println();
+            System.out.println("Enter two numbers (or x to exit): ");
+            try {
+                System.out.print("first: ");
+                first = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.print("Enter an operator (+, -, *, /): ");
+                operatorSymbol = scanner.nextLine().charAt(0);
+                o = Operator.findBySymbol(operatorSymbol);
+                if ( o == null ) continue;  // operator not recognized
+
+                System.out.print("second: ");
+                second = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.println(first + " " + operatorSymbol + " " + second + " = " + o.calculate(first,second));
+            } catch (Exception e) {
+                String buffer = scanner.nextLine();
+                if (buffer.toUpperCase().equals("X")) {
+                    System.exit(0);
+                }
+                System.out.println(buffer + " is not valid number, try again ..");
+                continue;
+            }
+
         }
-        System.out.printf(first + " " + operator + " " + second + " = " + result);
+
     }
+
 }
